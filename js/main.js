3dfbcba6ea39a4fac46406dc48ed6c898e6c0bf9
dennis-mays <dennis.mays@gmail.com>
2022-04-03 19:58:06 -0700

    added cuepoints

--- js/main.js
+++ js/main.js
@@ -12,19 +12,30 @@ import { cueTimer } from "./modules/cuepoints.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
     var myCues = [
-        { seconds: 4, callback: func1 },
-        { seconds: 9, callback: func2 },
-        { seconds: 31, callback: func3 },
-        { seconds: 42, callback: func4 },
-        { seconds: 50, callback: func5 },
-        { seconds: 60, callback: clearWindow },
-        { seconds: 80, callback: func7 },
-        { seconds: 86, callback: func8 },
-        { seconds: 99, callback: func9 },
-        { seconds: 107, callback: clearWindow },
-        { seconds: 119, callback: func10 },
-        { seconds: 137, callback: clearWindow },
-        { seconds: 170, callback: func11 },
+        { seconds: 3, callback: func1 },
+        { seconds: 5, callback: func2 },
+        { seconds: 6, callback: func3 },
+        { seconds: 8, callback: func4 },
+        { seconds: 9, callback: clearBotFrame },
+        { seconds: 11, callback: clearTopFrame },
+        { seconds: 13, callback: func5 },
+        { seconds: 15, callback: func6 },
+        { seconds: 23, callback: clearAll },
+        { seconds: 27, callback: func7 },
+        { seconds: 37, callback: func8 },
+        { seconds: 47, callback: clearTopFrame },
+        { seconds: 51, callback: func9 },
+        { seconds: 55, callback: func10 },
+        { seconds: 67, callback: func11 },
+        { seconds: 80, callback: clearAll },
+        { seconds: 82, callback: func12 },
+        { seconds: 85, callback: func13 },
+        { seconds: 90, callback: func14 },
+        { seconds: 114, callback: func15 },
+        { seconds: 100, callback: func16 },
+        { seconds: 106, callback: func17 },
+        { seconds: 120, callback: clearAll },
+        { seconds: 178, callback: func18 },
     ];
 
     // This activates the cuepoints module.
@@ -46,50 +57,97 @@ document.addEventListener("DOMContentLoaded", (e) => {
 // You can code up whatever behavior you need in your own callbacks
 // feel free to rename the functions to be more descriptive of what they do.
 
+function clearTopFrame() {
+    document.querySelector("#topFrame").src = "./blank.html";
+}
+
+function clearBotFrame() {
+    document.querySelector("#botFrame").src = "./blank.html";
+}
+
+function clearAll() {
+    document.querySelector("#topFrame").src = "./blank.html";
+    document.querySelector("#botFrame").src = "./blank.html";
+}
+
+// function func1() {
+//     document.querySelector("#topFrame").src = "images/image01.jpg";
+//     document.querySelector("#botFrame").style.background =
+//         "url(images/blueprints.jpg) no-repeat top center";
+//     document.querySelector("#botFrame").style.backgroundSize = "contain";
+// }
+
 function func1() {
-    document.querySelector("#vid").style = "outline : 3px solid #6441a5";
+    document.querySelector("#botFrame").src = "images/apollo.jpg";
 }
 
 function func2() {
-    document.querySelector("#web").src =
-        "https://en.wikipedia.org/wiki/San_Francisco_Bay";
+    document.querySelector("#topFrame").src = "images/shuttle.jpg";
 }
 
 function func3() {
-    document.querySelector("#web").src =
-        "https://en.wikipedia.org/wiki/San_Pablo_Bay";
+    document.querySelector("#botFrame").src = "images/iss.png";
 }
 
 function func4() {
-    document.querySelector("#web").src =
-        "https://en.wikipedia.org/wiki/Carquinez_Strait";
+    document.querySelector("#topFrame").src = "images/hubble.jpg";
 }
 
 function func5() {
-    document.querySelector("#web").src = "images/river_delta.jpg";
+    document.querySelector("#topFrame").src = "images/image01.jpg";
 }
 
-function clearWindow() {
-    document.querySelector("#web").src = "./blank.html";
+function func6() {
+    document.querySelector("#botFrame").src = "images/blueprints.jpg";
 }
 
 function func7() {
-    document.querySelector("#web").src = "images/farallon_islands.jpg";
+    document.querySelector("#botFrame").src =
+        "https://en.wikipedia.org/wiki/James_Webb_Space_Telescope";
+    document.querySelector("#topFrame").src = "images/image02.png";
 }
 
 function func8() {
-    document.querySelector("#web").src = "images/14000coastline.jpg";
+    document.querySelector("#topFrame").src = "images/spof.png";
 }
 
 function func9() {
-    document.querySelector("#web").src = "images/wooly.jpg";
+    document.querySelector("#topFrame").src = "images/size.png";
 }
 
 function func10() {
-    document.querySelector("#web").src =
-        "https://en.wikipedia.org/wiki/Gulf_of_the_Farallones";
+    document.querySelector("#botFrame").src =
+        "https://en.wikipedia.org/wiki/Big_Bang";
 }
 
 function func11() {
-    document.querySelector("#web").src = "images/sfbay.jpg";
+    document.querySelector("#topFrame").src = "images/jwst02.png";
+}
+
+function func12() {
+    document.querySelector("#botFrame").src = "images/ff03.png";
+}
+
+function func13() {
+    document.querySelector("#topFrame").src = "images/ff02.png";
+}
+
+function func14() {
+    document.querySelector("#botFrame").src = "images/ff01.png";
+}
+
+function func15() {
+    document.querySelector("#topFrame").src = "images/image06.png";
+}
+
+function func16() {
+    document.querySelector("#botFrame").src = "images/image03.png";
+}
+
+function func17() {
+    document.querySelector("#topFrame").src = "images/image05.jpg";
+}
+
+function func18() {
+    document.querySelector("#botFrame").src = "https://www.stsci.edu/";
 }
