c02d990b92b5689fab7bbc774e0a0abc8374ddd6
dennis-mays <dennis.mays@gmail.com>
2022-04-11 15:45:24 -0700

    code and styling changes

--- js/main.js
+++ js/main.js
@@ -12,46 +12,46 @@ import { cueTimer } from "./modules/cuepoints.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
     var myCues = [
-        { seconds: 3, callback: func1 },
-        { seconds: 5, callback: func2 },
-        { seconds: 6, callback: func3 },
-        { seconds: 8, callback: func4 },
+        { seconds: 3, callback: func1 }, // Apollo mission
+        { seconds: 5, callback: func2 }, // Space Shuttle
+        { seconds: 6, callback: func3 }, // Int Space Station
+        { seconds: 8, callback: func4 }, // Hubble
         { seconds: 9, callback: clearBotFrame },
         { seconds: 11, callback: clearTopFrame },
-        { seconds: 13, callback: func5 },
-        { seconds: 15, callback: func6 },
+        { seconds: 13, callback: func5 }, // Webb logo on rocket
+        { seconds: 15, callback: func6 }, // Webb blueprints
         { seconds: 23, callback: clearAll },
-        { seconds: 27, callback: func7 },
-        { seconds: 37, callback: func8 },
+        { seconds: 27, callback: func7 }, // Webb Wikipedia page
+        // { seconds: 37, callback: func8 },
         { seconds: 47, callback: clearTopFrame },
-        { seconds: 51, callback: func9 },
-        { seconds: 55, callback: func10 },
-        { seconds: 67, callback: func11 },
+        { seconds: 51, callback: func9 }, // Telescope size comparision image
+        { seconds: 65, callback: func10 }, // Big Bang article web page
+        // { seconds: 67, callback: func11 },
         { seconds: 80, callback: clearAll },
-        { seconds: 84, callback: func23 },
-        { seconds: 100, callback: func16 },
-        { seconds: 106, callback: func17 },
-        { seconds: 114, callback: func15 },
+        { seconds: 84, callback: func23 }, // Mirror size comparison
+        { seconds: 100, callback: func16 }, // Rocket on launch pad
+        { seconds: 106, callback: func17 }, // Rocket liftoff
+        { seconds: 114, callback: func15 }, // Webb folded with fairing
         { seconds: 120, callback: clearAll },
-        { seconds: 125, callback: func19 },
-        { seconds: 178, callback: func18 },
-        { seconds: 263, callback: func20 },
-        { seconds: 273, callback: func27 },
-        { seconds: 277, callback: func24 },
-        { seconds: 290, callback: func21 },
+        { seconds: 125, callback: func19 }, // Deployment video
+        { seconds: 178, callback: func18 }, // Space Telescope Science Institute webpage
+        { seconds: 263, callback: func20 }, // Orbit distance image
+        { seconds: 273, callback: func27 }, // Orbit illustration image
+        { seconds: 277, callback: func24 }, // Orbit video
+        { seconds: 290, callback: func21 }, // Solar Wind explanation
         { seconds: 296, callback: clearTopFrame },
-        { seconds: 297, callback: func12 },
-        { seconds: 306, callback: func31 },
-        { seconds: 320, callback: func22 },
+        { seconds: 297, callback: func12 }, // Webb fun fact primary mirror
+        { seconds: 306, callback: func31 }, // ESA website on sunshield
+        // { seconds: 320, callback: func22 }, // Kapton definition
         { seconds: 330, callback: clearBotFrame },
-        { seconds: 331, callback: func26 },
-        { seconds: 365, callback: clearTopFrame },
-        { seconds: 390, callback: func25 },
-        { seconds: 393, callback: func28 },
-        { seconds: 410, callback: func32 },
+        { seconds: 331, callback: func26 }, // Time lapse video
+        // { seconds: 365, callback: clearTopFrame },
+        { seconds: 390, callback: func25 }, // Informational image
+        { seconds: 400, callback: func28 }, // Informational image
+        { seconds: 410, callback: func32 }, // Informational image
         { seconds: 420, callback: clearAll },
-        { seconds: 421, callback: func30 },
-        { seconds: 460, callback: func33 },
+        { seconds: 421, callback: func30 }, // Webb instrumentation website and image
+        { seconds: 460, callback: func33 }, // Webb logo
     ];
 
     // This activates the cuepoints module.
@@ -73,6 +73,24 @@ document.addEventListener("DOMContentLoaded", (e) => {
 // You can code up whatever behavior you need in your own callbacks
 // feel free to rename the functions to be more descriptive of what they do.
 
+// Function to set one iframe in col2
+function setOneFrame() {
+    document.querySelector(".col2").innerHTML =
+        '<iframe id="topFrame" src="./blank.html" allow="autoplay"></iframe>';
+    document.querySelector("#topFrame").style.height = "88vh";
+}
+
+// Function to set two iframes in col2
+function setTwoFrame() {
+    setOneFrame();
+    document.querySelector(".col2").innerHTML +=
+        '<iframe id="botFrame" src="./blank.html" allow="autoplay"></iframe>';
+    // document.querySelector(".col2").innerHTML =
+    //     '<iframe id="topFrame" src="./blank.html" allow="autoplay"></iframe><iframe id="botFrame" src="./blank.html" allow="autoplay"></iframe>';
+    document.querySelector("#topFrame").style.height = "44vh";
+    document.querySelector("#botFrame").style.height = "44vh";
+}
+
 function clearTopFrame() {
     document.querySelector("#topFrame").src = "./blank.html";
 }
@@ -87,6 +105,7 @@ function clearAll() {
 }
 
 function func1() {
+    setTwoFrame();
     document.querySelector("#botFrame").src = "images/apollo.jpg";
 }
 
@@ -111,30 +130,33 @@ function func6() {
 }
 
 function func7() {
-    document.querySelector("#botFrame").src =
+    setOneFrame();
+    document.querySelector("#topFrame").src =
         "https://en.wikipedia.org/wiki/James_Webb_Space_Telescope";
-    document.querySelector("#topFrame").src = "images/image02.png";
+    // document.querySelector("#topFrame").style.height = "88vh";
 }
 
-function func8() {
-    document.querySelector("#topFrame").src = "images/spof.png";
-}
+// function func8() {
+//     document.querySelector("#topFrame").src = "images/spof.png";
+// }
 
 function func9() {
-    document.querySelector("#topFrame").src = "images/size.png";
+    setOneFrame();
+    document.querySelector("#topFrame").src = "images/jwst_comparison.png";
 }
 
 function func10() {
-    document.querySelector("#botFrame").src =
-        "https://en.wikipedia.org/wiki/Big_Bang";
+    document.querySelector("#topFrame").src =
+        "https://bigthink.com/starts-with-a-bang/big-bang-beginning-universe/";
 }
 
-function func11() {
-    document.querySelector("#topFrame").src = "images/jwst02.png";
-}
+// function func11() {
+//     document.querySelector("#topFrame").src = "images/jwst02.png";
+// }
 
 function func12() {
-    document.querySelector("#botFrame").src = "images/ff03.png";
+    setOneFrame();
+    document.querySelector("#topFrame").src = "images/ff03.jpg";
 }
 
 // function func13() {
@@ -150,6 +172,7 @@ function func15() {
 }
 
 function func16() {
+    setTwoFrame();
     document.querySelector("#botFrame").src = "images/image03.png";
 }
 
@@ -175,12 +198,13 @@ function func21() {
     document.querySelector("#botFrame").src = "images/solarwind.png";
 }
 
-function func22() {
-    document.querySelector("#botFrame").src = "images/kapton.png";
-}
+// function func22() {
+//     document.querySelector("#botFrame").src = "images/kapton.png";
+// }
 
 function func23() {
-    document.querySelector("#topFrame").src = "images/image08.png";
+    setOneFrame();
+    document.querySelector("#topFrame").src = "images/size.jpg";
 }
 
 function func24() {
@@ -189,13 +213,14 @@ function func24() {
 }
 
 function func25() {
+    setTwoFrame();
     document.querySelector("#topFrame").src = "images/jwst04.png";
 }
 
 function func26() {
-    document.querySelector("#botFrame").src =
+    document.querySelector("#topFrame").src =
         "https://www.dennismays.com/artdm174/media/jwst_sunshield720.mp4?autoplay=1";
-    document.querySelector("#botFrame").type = "video/mp4";
+    document.querySelector("#topFrame").type = "video/mp4";
 }
 
 function func27() {
@@ -213,8 +238,9 @@ function func28() {
 function func30() {
     document.querySelector("#topFrame").src =
         "https://www.stsci.edu/jwst/instrumentation";
-    document.querySelector("#botFrame").src =
-        "https://www.wevolver.com/article/james-webb-space-telescope-science-instruments-explained";
+    document.querySelector("#botFrame").src = "images/jwst.jpg";
+    // document.querySelector("#botFrame").src =
+    //     "https://www.wevolver.com/article/james-webb-space-telescope-science-instruments-explained";
 }
 
 function func31() {
@@ -227,7 +253,8 @@ function func32() {
 }
 
 function func33() {
-    document.querySelector("#botFrame").src =
-        "https://en.wikipedia.org/wiki/James_Webb_Space_Telescope";
+    setOneFrame();
+    // document.querySelector("#botFrame").src =
+    //     "https://en.wikipedia.org/wiki/James_Webb_Space_Telescope";
     document.querySelector("#topFrame").src = "images/webblogo.png";
 }
